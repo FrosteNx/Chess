52df5ea2dd68586bcb3b52a36d6ae56a0ec4f77f
FrosteNx <domr1a@wp.pl>
2024-04-23 02:28:27 +0200

    pawn promotion

---
+++ ChessUI/PromotionMenu.xaml.cs
@@ -0,0 +1,39 @@
+﻿using ChessLogic;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ChessUI
+{
+    /// <summary>
+    /// Logika interakcji dla klasy PromotionMenu.xaml
+    /// </summary>
+    public partial class PromotionMenu : UserControl
+    {
+        public event Action<PieceType> PieceSelected;
+
+        public PromotionMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void QueenImg_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void RookImg_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void BishopImg_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void KnightImg_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
